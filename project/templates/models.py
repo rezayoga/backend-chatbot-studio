1cfdf9c55a4d6716f90b4f1a28b0fed25a509214
Reza Yogaswara <reza.yoga@gmail.com>
2022-09-26 09:09:57 +0700

    Updating

--- project/templates/models.py
+++ project/templates/models.py
@@ -18,7 +18,7 @@ class User(Base):
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
-    templates = relationship("Template", backref="owner")
+    templates = relationship("Template", backref="template_owner")
 
     def __repr__(self):
         return f"{self.name} <{self.email}>"

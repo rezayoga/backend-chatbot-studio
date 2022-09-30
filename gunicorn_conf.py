5a7c76d83ddf370bc8ccc221d8f18d5fbea453c0
Reza Yogaswara <reza.yoga@gmail.com>
2022-09-30 18:38:09 +0700

    Updating

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -8,7 +8,8 @@ workers = cpu_count() + 1
 worker_class = 'uvicorn.workers.UvicornWorker'
 
 # Logging Options
-loglevel = 'info'
 accesslog = '/home/rezayogaswara/python_projects/backend-chatbot-studio/access_log'
 errorlog = '/home/rezayogaswara/python_projects/backend-chatbot-studio/error_log'
 capture_output = True
+loglevel = 'info'
+reload = True

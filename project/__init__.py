090d706a75fb5bffd6e1341f8d22d2f87f33903b
Reza Yogaswara <reza.yoga@gmail.com>
2022-09-30 15:10:21 +0700

    Updating

--- project/__init__.py
+++ project/__init__.py
@@ -10,10 +10,7 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
+        allow_origins=origins
     )
 
     def custom_openapi():

7014022a6f2533125b694b8892a708b42b3efe8c
Reza Yogaswara <reza.yoga@gmail.com>
2022-09-30 15:19:29 +0700

    Updating

--- project/__init__.py
+++ project/__init__.py
@@ -1,17 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 from project.celery_utils import create_celery   # new
 from fastapi.openapi.utils import get_openapi
-from starlette.middleware import Middleware
-from starlette.middleware.cors import CORSMiddleware
 
 
 def create_app() -> FastAPI:
-    origins = ["http://localhost", "http://localhost:5173"]
 
-    middleware = [
-        Middleware(CORSMiddleware, allow_origins=origins)
-    ]
-    app = FastAPI(middleware=middleware)
+    app = FastAPI()
+
+    # Salt to your taste
+    ALLOWED_ORIGINS = 'localhost:5173'    # or 'foo.com', etc.
+
+    # handle CORS preflight requests
+    @app.options('/{rest_of_path:path}')
+    async def preflight_handler(request: Request, rest_of_path: str) -> Response:
+        response = Response()
+        response.headers['Access-Control-Allow-Origin'] = ALLOWED_ORIGINS
+        response.headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type'
+        return response
+
+    # set CORS headers
+    @app.middleware("http")
+    async def add_CORS_header(request: Request, call_next):
+        response = await call_next(request)
+        response.headers['Access-Control-Allow-Origin'] = ALLOWED_ORIGINS
+        response.headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type'
+        return response
 
     def custom_openapi():
         if app.openapi_schema:

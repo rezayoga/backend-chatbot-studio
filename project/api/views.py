b2d207a511c1d8d67a37f926deca303def34e52f
Reza Yogaswara <reza.yoga@gmail.com>
2022-10-04 10:10:13 +0700

    Updating

--- project/api/views.py
+++ project/api/views.py
@@ -186,7 +186,7 @@ async def create_template(created_template: TemplateSchema, user: dict = Depends
     session.add(template)
     session.commit()
 
-    return JSONResponse(status_code=200, content={"message": "Template created successfully", "data": jsonable_encoder(template)})
+    return JSONResponse(status_code=200, content={"message": "Template created successfully", "data": template})
 
 
 @api_router.get("/templates/{template_id}/", tags=["templates"])

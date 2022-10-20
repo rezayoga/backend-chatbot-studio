675c347f4f7f943e17e5bfe0b9a475483ba47dad
Reza Yogaswara <reza.yoga@gmail.com>
2022-10-20 21:24:47 +0700

    Updating

--- project/config.py
+++ project/config.py
@@ -1,39 +1,37 @@
-import os
 from functools import lru_cache
 
 
 class BaseConfig:
-    # DATABASE_URL: str = "postgresql+asyncpg://reza:reza@localhost/db_chatbot_studio"
-    DATABASE_URL: str = "postgresql+asyncpg://reza:reza@103.41.204.222/db_chatbot_studio"
-    DATABASE_CONNECT_DICT: dict = {}
+	# DATABASE_URL: str = "postgresql+asyncpg://reza:reza@localhost/db_chatbot_studio"
+	DATABASE_URL: str = "postgresql+asyncpg://reza:reza@103.41.204.222/db_chatbot_studio"
+	DATABASE_CONNECT_DICT: dict = {}
 
-    CELERY_BROKER_URL: str = "redis://127.0.0.1:6379/0"
-    CELERY_RESULT_BACKEND: str = "redis://127.0.0.1:6379/0"
+	CELERY_BROKER_URL: str = "redis://127.0.0.1:6379/0"
+	CELERY_RESULT_BACKEND: str = "redis://127.0.0.1:6379/0"
 
 
 class DevelopmentConfig(BaseConfig):
-    pass
+	pass
 
 
 class ProductionConfig(BaseConfig):
-    pass
+	pass
 
 
 class TestingConfig(BaseConfig):
-    pass
+	pass
 
 
 @lru_cache()
 def get_settings():
-    config_cls_dict = {
-        "development": DevelopmentConfig,
-        "production": ProductionConfig,
-        "testing": TestingConfig
-    }
+	config_cls_dict = {
+		"development": DevelopmentConfig,
+		"production": ProductionConfig,
+		"testing": TestingConfig
+	}
 
-    config_name = "development"
-    config_cls = config_cls_dict[config_name]
-    return config_cls()
-
+	config_name = "development"
+	config_cls = config_cls_dict[config_name]
+	return config_cls()
 
 settings = get_settings()

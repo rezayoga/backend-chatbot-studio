09a1a2b09c014215c70eff16657da415e2de710b
Reza Yogaswara <reza.yoga@gmail.com>
2022-10-20 21:29:42 +0700

    Updating

--- project/config.py
+++ project/config.py
@@ -2,8 +2,8 @@ from functools import lru_cache
 
 
 class BaseConfig:
-	# DATABASE_URL: str = "postgresql+asyncpg://reza:reza@localhost/db_chatbot_studio"
-	DATABASE_URL: str = "postgresql+asyncpg://reza:reza@103.41.204.222/db_chatbot_studio"
+	DATABASE_URL: str = "postgresql+asyncpg://reza:reza@localhost/db_chatbot_studio"
+	# DATABASE_URL: str = "postgresql+asyncpg://reza:reza@103.41.204.222/db_chatbot_studio"
 	DATABASE_CONNECT_DICT: dict = {}
 
 	CELERY_BROKER_URL: str = "redis://127.0.0.1:6379/0"
@@ -33,5 +33,6 @@ def get_settings():
 	config_name = "development"
 	config_cls = config_cls_dict[config_name]
 	return config_cls()
+
 
 settings = get_settings()
